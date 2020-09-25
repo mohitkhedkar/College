15c34e2c7cf392c9a13727826d2141fa344ff164
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-09-25 20:36:46 +0530

    Assignment 1 Added

---
+++ 2 Year/OOPL/Assignment 1.cpp
@@ -0,0 +1,8 @@
+// Implement a class Complex which represents the Complex Number data type.
+// Implement the following operations:
+// 1.Constructor (including a default constructor which creates the complex number 0+0i).
+// 2.Overloaded operator+ to add two complex numbers.
+// 3.Overloaded operator* to multiply two complex numbers.
+// 4.Overloaded << and >> to print and read Complex Numbers. 
+
+// Solved by: Mohit Khedkar
