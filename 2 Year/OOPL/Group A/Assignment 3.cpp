93d9d3d30b66a81d381e01186995818742a441c5
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-10-07 22:24:44 +0530

    OOPL Update

---
+++ 2 Year/OOPL/Group A/Assignment 3.cpp
@@ -0,0 +1,17 @@
+// Implement a class Quadratic that represents degree two polynomials i.e polynomials of type ax2+bx+c. The class will require three data members corresponding to a, b and c. Implement the following operations:
+// 1. A constructor (including a default constructor which creates the 0 polynomial).
+// 2. Overloaded operator+ to add two polynomials of degree 2.
+// 3. Overloaded << and >> to print and read polynomials. To do this, you will need to decide what you want your input and output format to look like.
+// 4. A function eval that computes the value of apolynomial for a given value of x.
+// 5. A function that computes the two solutions of the equation ax2+bx+c=0.
+
+// Author: Mohit Khedkar
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
