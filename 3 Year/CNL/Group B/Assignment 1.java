047e0769562efb4d7df826272d2ea29a42c51ef9
mohitkhedkar <mohitkhedkar521@gmail.com>
2020-10-18 20:59:08 +0530

    Update

---
+++ 3 Year/CNL/Group B/Assignment 1.java
@@ -0,0 +1,7 @@
+// Write a Program to transfer- By using Bluetooth (Use JAVA/PYTHON)
+// 1. Characters separated by space
+// 2. One Strings at a time
+// 3. One Sentence at a time 
+// 4. File 
+
+// Author: Mohit Khedkar.
