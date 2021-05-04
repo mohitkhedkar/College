c836659e77ce96fa4b5d75c26d99c4ece84d377c
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-05-04 12:02:10 +0530

    :tada: Assignment 5

--- 3 Year/CNL/Group A/Assignment 2.cpp
+++
@@ -1,7 +0,0 @@
-//  Write a Program with following four options to transfer
-// 1. Characters separated by space 
-// 2. One Strings at a time
-// 3. One Sentence at a time 
-// 4. file between two RS 232D or USB ports.(Use C/C++)  
-
-// Author: Mohit Khedkar.
